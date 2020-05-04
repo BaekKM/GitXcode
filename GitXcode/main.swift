9b35f93404ce18b0060a3e32b7f7d68a1865fc7c
KMBaek <bsh6403@naver.com>
2020-05-04 14:23:15 +0900

    Initial Commit

---
+++ GitXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode
+//
+//  Created by IOS on 2020/05/04.
+//  Copyright © 2020 Baek Kyoung Min. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
